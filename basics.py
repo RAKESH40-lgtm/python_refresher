08a00d51c24980c73e253ddaf5fccf8aa647add4
RAKESH40-lgtm <ram19042k@protonmail.com>
2026-07-03 19:03:53 +0530

    Python refresher 1

---
+++ basics.py
@@ -0,0 +1,25 @@
+# For Python refreshing 
+line1 ="*******************"
+line2 ="*                 *"
+line3 = "Python refresher"
+
+#Used to print
+# print("")
+# print(line1)
+# print(line2)
+# print(line2)
+# print(line3)
+# print(line1)
+
+#Basic operator 
+age = 20
+
+#condition check
+# if age > 18 :
+#     print("Elgible for vote")
+# else :
+#     print("Not elgible")
+
+#Ternary operator (true print statement conditionexpression else false print statement)
+
+print("Elgible for Vote ") if age > 18 else print("Not elgible ")
