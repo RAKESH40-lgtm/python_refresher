d4fd4be06d91d7e33d021200664dd1cded6349a3
RAKESH40-lgtm <ram19042k@protonmail.com>
2026-07-03 19:24:52 +0530

    datatypes updated

---
+++ data_types.py
@@ -0,0 +1,45 @@
+#datatype -string
+first_name = " Rakesh    "
+last_name = "sharman"
+
+# multiline
+address = """
+ XYZ ,
+ LMN     ORM
+
+"""
+#str constructor for casting one type to another
+use_city = str("Yelahanka")
+use_pincode = str(12344) 
+
+
+
+#String methods
+# print(first_name.lower())
+# print(first_name.upper())
+# print(address.title())
+# print(len(first_name)) 
+# print(type(address))
+# print(isinstance(address,str))
+# print(first_name + " " +  last_name)
+# print(first_name.replace("Rakesh","rakesh 0"))
+# print(use_pincode)
+# print(use_city)
+# print(first_name)
+# print(first_name.strip())
+
+# integer
+age=10
+
+# print(abs(-19))
+# print(round(1.00))
+# print(float(2.0))
+
+#Boolean
+isValid= True
+status = False
+# print(bool(isValid))
+# print(bool(isValid))
+# print(isValid and status)
+# print(isValid or status)
+# print(not isValid)
